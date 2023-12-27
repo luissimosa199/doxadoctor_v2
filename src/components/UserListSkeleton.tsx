8516d09b85cb4f321b1ecc6ecdeaddee0397eb5d
Luis <simosa37@gmail.com>
2023-12-27 12:05:54 -0300

    fix: doctors list skeleton

--- src/components/UserListSkeleton.tsx
+++ src/components/UserListSkeleton.tsx
@@ -3,28 +3,26 @@ import UserFilterContainerSkeleton from "./UserFilterContainerSkeleton";
 
 const UserListSkeleton = () => {
   return (
-    <div
-      className={`min-h-[130vh] px-6 rounded-lg shadow-md max-w-[450px] md:max-w-[850px] lg:max-w-4xl xl:max-w-6xl mx-auto`}
-    >
-      <div className="w-full">
+    <div className={`min-h-[400vh] px-6`}>
+      <div className="w-full max-w-[450px] md:max-w-[850px] lg:max-w-4xl xl:max-w-6xl mx-auto">
         <UserFilterContainerSkeleton />
 
         <div className="p-2 h-10 mt-4 w-full border rounded" />
       </div>
 
-      <ul className="flex flex-wrap gap-y-4 justify-around w-full">
-        <li className="h-[180px] w-[180px] bg-gray-300 my-2 animate-pulse"></li>
-        <li className="h-[180px] w-[180px] bg-gray-300 my-2 animate-pulse"></li>
-        <li className="h-[180px] w-[180px] bg-gray-300 my-2 animate-pulse"></li>
-        <li className="h-[180px] w-[180px] bg-gray-300 my-2 animate-pulse"></li>
-        <li className="h-[180px] w-[180px] bg-gray-300 my-2 animate-pulse"></li>
-        <li className="h-[180px] w-[180px] bg-gray-300 my-2 animate-pulse"></li>
-        <li className="h-[180px] w-[180px] bg-gray-300 my-2 animate-pulse"></li>
-        <li className="h-[180px] w-[180px] bg-gray-300 my-2 animate-pulse"></li>
-        <li className="h-[180px] w-[180px] bg-gray-300 my-2 animate-pulse"></li>
-        <li className="h-[180px] w-[180px] bg-gray-300 my-2 animate-pulse"></li>
-        <li className="h-[180px] w-[180px] bg-gray-300 my-2 animate-pulse"></li>
-        <li className="h-[180px] w-[180px] bg-gray-300 my-2 animate-pulse"></li>
+      <ul className="flex flex-wrap gap-y-6 justify-around w-full">
+        <li className="h-[400px] w-[280px] bg-gray-300 my-2 animate-pulse"></li>
+        <li className="h-[400px] w-[280px] bg-gray-300 my-2 animate-pulse"></li>
+        <li className="h-[400px] w-[280px] bg-gray-300 my-2 animate-pulse"></li>
+        <li className="h-[400px] w-[280px] bg-gray-300 my-2 animate-pulse"></li>
+        <li className="h-[400px] w-[280px] bg-gray-300 my-2 animate-pulse"></li>
+        <li className="h-[400px] w-[280px] bg-gray-300 my-2 animate-pulse"></li>
+        <li className="h-[400px] w-[280px] bg-gray-300 my-2 animate-pulse"></li>
+        <li className="h-[400px] w-[280px] bg-gray-300 my-2 animate-pulse"></li>
+        <li className="h-[400px] w-[280px] bg-gray-300 my-2 animate-pulse"></li>
+        <li className="h-[400px] w-[280px] bg-gray-300 my-2 animate-pulse"></li>
+        <li className="h-[400px] w-[280px] bg-gray-300 my-2 animate-pulse"></li>
+        <li className="h-[400px] w-[280px] bg-gray-300 my-2 animate-pulse"></li>
       </ul>
     </div>
   );
